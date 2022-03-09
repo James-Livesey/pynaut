444529e91b7022fc79abbb1d6dd6ea0dd8daf569
James-Livesey <jamesliveg@gmail.com>
2022-03-09 19:55:19 +0000

    Add editor JS implementation

---
+++ editor/editor.js
@@ -0,0 +1,81 @@
+export class Editor {
+    constructor(element) {
+        this.element = element;
+
+        this.attachEvents();
+    }
+
+    get code() {
+        return [...this.element.querySelectorAll("div[pynaut-line]")].map((element) => element.textContent).join("\n");
+    }
+
+    set code(value) {
+        var thisScope = this;
+
+        this.element.querySelector("pynaut-code").innerHTML = "";
+
+        value.split("\n").forEach(function(line) {
+            var lineElement = document.createElement("div");
+
+            lineElement.setAttribute("pynaut-line", "");
+
+            lineElement.innerText = line;
+
+            thisScope.element.querySelector("pynaut-code").append(lineElement);
+        });
+    }
+
+    clean() {
+        if (
+            this.element.querySelector("div[pynaut-line]") == null ||
+            [...this.element.querySelector("pynaut-code").childNodes].some((node) => node.nodeType == Node.TEXT_NODE && node.nodeValue.trim() != "")
+        ) {
+            var lineElement = document.createElement("div");
+
+            lineElement.setAttribute("pynaut-line", "");
+
+            lineElement.textContent = this.element.querySelector("pynaut-code").textContent;
+
+            this.element.querySelector("pynaut-code").innerHTML = "";
+
+            this.element.querySelector("pynaut-code").append(lineElement);
+
+            var range = document.createRange();
+
+            range.setStart(lineElement, lineElement.textContent.length);
+            range.collapse(true);
+
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+        }
+    }
+
+    attachEvents() {
+        var thisScope = this;
+
+        this.element.querySelector("pynaut-code").addEventListener("paste", function(event) {
+            event.preventDefault();
+
+            thisScope.clean();
+
+            var htmlToInsert = event.clipboardData.getData("text/plain")
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/\n/g, "</div><div pynaut-line>")
+            ;
+
+            if (window.getSelection().toString().trim().length == thisScope.code.length) {
+                thisScope.code = event.clipboardData.getData("text/plain");
+
+                return;
+            }
+
+            document.execCommand("insertHTML", false, htmlToInsert);
+        });
+
+        this.element.querySelector("pynaut-code").addEventListener("keydown", function() {
+            thisScope.clean();
+        });
+    }
+}
--- editor/script.js
+++ editor/script.js
@@ -1,4 +1,8 @@
+import * as editor from "./editor.js";
+
 window.addEventListener("load", function() {
+    var mainEditor = new editor.Editor(document.querySelector("#editor"));
+
     document.querySelector("#runButton").addEventListener("click", function() {
         Sk.configure({
             output: (data) => document.querySelector("#console textarea").value += data,
@@ -7,7 +11,7 @@ window.addEventListener("load", function() {
         });
 
         Sk.misceval.asyncToPromise(function() {
-            return Sk.importMainWithBody("<stdin>", false, [...document.querySelectorAll("#editor pynaut-line")].map((element) => element.textContent).join("\n"), true);
+            return Sk.importMainWithBody("<stdin>", false, mainEditor.code, true);
         });
     });
 });
